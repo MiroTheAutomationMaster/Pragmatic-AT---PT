2936309ae2a85781952583b3b3f2191dd5841ba7
MiroTheAutomationMaster <mirokn@abv.bg>
2025-10-11 16:27:31 +0300

    Homework 4 - Typescript

---
+++ Homework4-Typescript.js
@@ -0,0 +1,18 @@
+{
+    var fullname = "Miroslav Stoimenov";
+    var age = 38;
+    console.log("Hello! I am ".concat(fullname, ", ").concat(age, " years old"));
+}
+{
+    var num1 = 8;
+    var num2 = 4;
+    var addition = num1 + num2;
+    var subtraction = num1 - num2;
+    console.log("Addition: ".concat(addition));
+    console.log("Subtraction: ".concat(subtraction));
+}
+{
+    var num = 12;
+    var isWithinRange = num >= 10 && num <= 15;
+    console.log("Is the number within range? ".concat(isWithinRange));
+}
