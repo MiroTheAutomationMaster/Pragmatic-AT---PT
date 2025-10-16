f02dc35f5d4a4b15811bf36a462697d679a4acf4
MiroTheAutomationMaster <mirokn@abv.bg>
2025-10-16 22:26:38 +0300

    Changes made

--- Homework4-Typescript.ts
+++ Homework4-Typescript.ts
@@ -1,13 +1,11 @@
 
-{
+
     let fullname: string = "Miroslav Stoimenov";
     let age: number = 38;
 
     console.log(`Hello! I am ${fullname}, ${age} years old`);
-}
 
 
-{
     let num1: number = 8;
     let num2: number = 4;
 
@@ -16,12 +14,9 @@
 
     console.log(`Addition: ${addition}`);
     console.log(`Subtraction: ${subtraction}`);
-}
 
 
-{
     let num: number = 12;
     let isWithinRange: boolean = num >= 10 && num <= 15;
 
     console.log(`Is the number within range? ${isWithinRange}`);
-}
